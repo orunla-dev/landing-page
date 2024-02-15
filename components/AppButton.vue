82021a0a1a8379f812ec197209828f84b0de87bc
= <=>
2024-02-15 23:44:39 +0100

    feat: AppHeader desktop version

---
+++ components/AppButton.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: () => {
+        return 'primary'
+      },
+    },
+  },
+}
+</script>
+<template>
+  <button
+    class="rounded-[8px] border border-primary font-semibold capitalize flex justify-center items-center px-3 py-2 md:px-10 md:py-3"
+    :class="type === 'primary' ? 'bg-primary text-white' : 'text-primary'"
+  >
+    <slot />
+  </button>
+</template>
