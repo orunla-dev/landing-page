818d4a0016cadddb169087d646d01cd1ca1e01e3
= <=>
2024-02-16 17:29:49 +0100

    feat: AppFooter desktop view

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -13,7 +13,7 @@ export default {
 <template>
   <button
     class="rounded-[8px] border border-primary font-semibold capitalize flex justify-center items-center px-3 py-2 md:px-10 md:py-3"
-    :class="type === 'primary' ? 'bg-primary text-white' : 'text-primary'"
+    :class="type === 'primary' ? 'bg-primary text-white' : 'text-primary bg-white'"
   >
     <slot />
   </button>
