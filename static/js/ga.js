8cb618cf5cb18ebae0ff22f61f0762e60786c614
= <=>
2024-03-05 19:51:52 +0100

    fix: Analytics

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,7 +1,7 @@
-// window.dataLayer = window.dataLayer || []
-// function gtag() {
-//   dataLayer.push(arguments)
-// }
-// gtag('js', new Date())
+window.dataLayer = window.dataLayer || []
+function gtag() {
+  dataLayer.push(arguments)
+}
+gtag('js', new Date())
 
-// gtag('config', 'G-L240M3DEG4')
+gtag('config', 'G-L240M3DEG4')
